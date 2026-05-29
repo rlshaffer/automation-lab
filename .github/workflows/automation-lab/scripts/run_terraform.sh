07925a5ba62cbac26da64f15e037401483920d44
Randy Shaffer <your.email@domain.com>
2026-05-29 07:16:52 -0400

    Removed GitHub workflow files

--- .github/workflows/automation-lab/scripts/run_terraform.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-terraform init
-
-terraform apply -auto-approve \
-  -var="vm_name=$VM_NAME" \
-  -var="vsphere_server=$VSPHERE_SERVER" \
-  -var="vsphere_user=$VSPHERE_USER" \
-  -var="vsphere_password=$VSPHERE_PASSWORD"
