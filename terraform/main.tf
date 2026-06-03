568839537035a32b943fb78f00e7f3b6979eeb87
Randy Shaffer <your.email@domain.com>
2026-06-03 16:13:25 -0400

    Test

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,13 +8,12 @@ terraform {
 
 
 provider "vsphere" {
-  user           = var.vsphere_user
-  password       = var.vsphere_password
-  vsphere_server = var.vsphere_server
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
 
   allow_unverified_ssl = true
 }
-
 
 data "vsphere_datacenter" "dc" {
   name = var.datacenter
